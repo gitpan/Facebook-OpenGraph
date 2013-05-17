b542c5f2894fd6f26c8cc279520f6416b858c5c2
Go Hagiwara <oklahomer@cpan.org>
2013-05-17 11:58:16 +0000

    Import of OKLAHOMER/Facebook-OpenGraph-0.03 from CPAN.
    gitpan-cpan-distribution: Facebook-OpenGraph
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         OKLAHOMER/Facebook-OpenGraph-0.03.tar.gz
    gitpan-cpan-author:       OKLAHOMER
    gitpan-cpan-maturity:     released

--- lib/Facebook/OpenGraph/Response.pm
+++ lib/Facebook/OpenGraph/Response.pm
@@ -2,23 +2,30 @@ package Facebook::OpenGraph::Response;
 use strict;
 use warnings;
 use Carp qw(croak);
-use JSON 2 qw(decode_json);
+use JSON 2 ();
 
 sub new {
     my $class = shift;
     my $args  = shift || +{};
 
     return bless +{
-        code    => $args->{code},
-        message => $args->{message},
-        content => $args->{content},
+        json        => $args->{json} || JSON->new->utf8,
+        headers     => $args->{headers},
+        code        => $args->{code},
+        message     => $args->{message},
+        content     => $args->{content},
+        req_headers => $args->{req_headers} || '',
+        req_content => $args->{req_content} || '',
     }, $class;
 }
 
 # accessors
-sub code    { shift->{code}    }
-sub message { shift->{message} }
-sub content { shift->{content} }
+sub code        { shift->{code}        }
+sub message     { shift->{message}     }
+sub content     { shift->{content}     }
+sub req_headers { shift->{req_headers} }
+sub req_content { shift->{req_content} }
+sub json        { shift->{json}        }
 
 sub is_success {
     my $self = shift;
@@ -31,25 +38,41 @@ sub is_success {
 sub error_string {
     my $self = shift;
     my $error = eval { $self->as_hashref->{error}; };
-    # sometimes error_subcode is not given
-    return $@ ? $self->message
-              : sprintf('%s:%s %s:%s', $error->{code}, $error->{error_subcode} || '-', $error->{type}, $error->{message});
+
+    my $err_str = '';
+    if ($@ || !$error) {
+        $err_str = $self->message;
+    }
+    else {
+        # sometimes error_subcode is not given
+        $err_str = sprintf(
+            '%s:%s %s:%s',
+            $error->{code},
+            $error->{error_subcode} || '-',
+            $error->{type},
+            $error->{message},
+        );
+    }
+
+    return $err_str;
 }
 
 sub as_json {
     my $self = shift;
     if ((my $bool = $self->content) =~ m/^(true|false)$/) {
-        # Sometimes they return plain text
-        # saying 'true' or 'false' to indicate result.
-        # So make it JSON formatted for our convinience.
+        # Sometimes they return plain text saying 'true' or 'false' to indicate
+        # result. So make it JSON formatted for our convinience. The key is
+        # named "success" so its format matches w/ some other endpoints that
+        # return {"success": "(true|false)"}.
         $self->{content} = sprintf('{"success" : "%s"}', $bool);
     };
     return $self->content; # content is JSON formatted
 }
 
 sub as_hashref {
+    my $self = shift;
     # just in case content is not properly formatted
-    my $hash_ref = eval { decode_json(shift->as_json); };
+    my $hash_ref = eval { $self->json->decode($self->as_json); };
     croak $@ if $@;
     return $hash_ref;
 }
@@ -69,7 +92,14 @@ Facebook::OpenGraph::Response - Response object for Facebook::OpenGraph.
 
 =head1 SYNOPSIS
 
-  my $res = Facebook::OpenGraph::Response->new($http_status_code, $http_status_message, $response_headers, $response_content);
+  my $res = Facebook::OpenGraph::Response->new(+{
+      code        => $http_status_code,
+      message     => $http_status_message,
+      headers     => $response_headers,
+      content     => $response_content,
+      req_headers => $req_headers,
+      req_content => $req_content,
+  });
 
 =head1 DESCRIPTION
 
@@ -79,40 +109,87 @@ This handles response object for Facebook::OpenGraph.
 
 =head2 Class Methods
 
-=head3 C<< Facebook::OpenGraph::Response->new($status_code :Int, $status_mess :Str, $headers :ArrayRef, $content :Str) :Object >>
+=head3 C<< Facebook::OpenGraph::Response->new(\%args) >>
 
 Creates and returns a new Facebook::OpenGraph::Response object.
 
+I<%args> can contain...
+
+=over 4
+
+=item * code
+
+HTTP status code
+
+=item * message
+
+HTTP status message
+
+=item * headers
+
+Response headers
+
+=item * content
+
+Response body
+
+=item * req_headers
+
+Stringified request headers
+
+=item * req_content
+
+Request content
+
+=back
+
 =head2 Instance Methods
 
-=head3 C<< $res->code() :Int >>
+=head3 C<< $res->code >>
 
 Returns HTTP status code
 
-=head3 C<< $res->message() :Str >>
+=head3 C<< $res->message >>
 
 Returns HTTP status message
 
-=head3 C<< $res->content() :Str >>
+=head3 C<< $res->content >>
 
 Returns response body
 
-=head3 C<< $res->is_success() :Bool >>
+=head3 C<< $res->req_headers >>
+
+Returns request header. This is especially useful for debugging. You must install the later 
+version of Furl to enable this or otherwise empty string will be returned.
+Also you have to specify Furl::HTTP->new(capture_request => 1) option.
+
+=head3 C<< $res->req_content >>
+
+Returns request body. This is especially useful for debugging. You must install the later 
+version of Furl to enable this or otherwise empty string will be returned.
+Also you have to specify Furl::HTTP->new(capture_request => 1) option.
+
+=head3 C<< $res->is_success >>
 
 Returns if status is 2XX or 304. 304 is added to handle $fb->fetch_with_etag();
 
-=head3 C<< $res->error_string() :Str >>
+=head3 C<< $res->error_string >>
 
 Returns error string.
 
-=head3 C<< $res->as_json() :Str >>
+=head3 C<< $res->as_json >>
 
-Returns response content as JSON string. Most of the time the response content itself is JSON formatted so it basically returns response content without doing anything. When Graph API returns plain text just saying 'true' or 'false,' it turns the content into JSON format like '{"success" : "(true|false)"}' so you can handle it in the same way as other case.
+Returns response content as JSON string. Most of the time the response content 
+itself is JSON formatted so it basically returns response content without doing 
+anything. When Graph API returns plain text just saying 'true' or 'false,' it 
+turns the content into JSON format like '{"success" : "(true|false)"}' so you 
+can handle it in the same way as other cases.
 
-=head3 C<< $res->as_hashref() :HashRef >>
+=head3 C<< $res->as_hashref >>
 
 Returns response content in hash reference.
 
-=head3 C<< $res->is_modified() :Bool >>
+=head3 C<< $res->is_modified >>
 
-Returns if target object is modified. This method is called in $fb->fetch_with_etag().
+Returns if target object is modified. This method is called in 
+$fb->fetch_with_etag().
